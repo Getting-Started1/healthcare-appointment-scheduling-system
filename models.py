707377380ca5b6a54d5edce8adda0c36bb766003
Elvis-Munene <104641808+Elvis-Munene@users.noreply.github.com>
2025-03-30 10:08:39 +0300

    feat: Add Patient, Doctor, Appointment, and MedicalRecord models

---
+++ models.py
@@ -0,0 +1,36 @@
+from tortoise.models import Model
+from tortoise import fields
+
+
+class Patient(Model):
+    id = fields.CharField(max_length=255)
+    name = fields.CharField(max_length=255, unique=True)
+    email = fields.CharField(max_length = 255, unique= True)
+    phone = fields.CharField(max_length = 20)
+    insurance_info = fields.TextField(null=True)
+    appointments = fields.ReverseRelation["Appointment"] # one to Many
+
+class Doctor(Model):
+    id = fields.IntField(pk=True)
+    name = fields.CharField(max_length= 255,unique=True)
+    specialization = fields.CharField(max_length= 255)
+    contact = fields.CharField(max_length= 20)
+    appointments = fields.ReverseRelation["Appointment"] # one to Many
+    
+class Appointment(Model):
+    id = fields.IntField(pk=True)
+    patient = fields.ForeignKeyField("models.Patient", related_name='appointments')
+    doctor =  fields.ForeignKeyField("models.Doctor", related_name="appintments")
+    start_time = fields.DatetimeField()
+    end_time = fields.DatetimeField()
+    status = fields.CharField(max_length =50, default="scheduled")
+    medical_record = fields.ReverseRelation["MedicalRecord"] # One to Many
+    
+class MedicalRecord(Model):
+    id = fields.IntField(pk=True)
+    patient = fields.ForeignKeyField("models.Patient", related_name="medical_record")
+    appointment = fields.ForeignKeyField("models.Appintment", related_name="medical_record")
+    diagnosis = fields.TextField()
+    prescription = fields.TextField()
+
+        
