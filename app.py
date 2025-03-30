5f7c4d9e29cfd2efa428e6b75b5c14f8f72222a3
Elvis-Munene <104641808+Elvis-Munene@users.noreply.github.com>
2025-03-30 10:08:06 +0300

    feat: Integrate Tortoise ORM for database

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from tortoise.contrib.fastapi import register_tortoise
+
 
 app = FastAPI()
 
@@ -6,4 +8,13 @@ app = FastAPI()
 
 def index():
     return {"Msg": "Hello World"}
+
+#Tortoise ORM setup
+register_tortoise(
+    app,
+    db_url="sqlite://db.sqlite3",
+    modules={"models": ["app.models"]},
+    generate_schemas=True,
+    add_exception_handlers=True,   
+)
 
