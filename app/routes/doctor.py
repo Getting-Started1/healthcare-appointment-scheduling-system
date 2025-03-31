f92a0bd42937cb3ee08d28cb55f5ad7e7337e135
Elvis-Munene <104641808+Elvis-Munene@users.noreply.github.com>
2025-03-31 23:51:05 +0300

    protects doctors routes

--- app/routes/doctor.py
+++ app/routes/doctor.py
@@ -1,21 +1,26 @@
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status
 from tortoise.exceptions import DoesNotExist
 from app.models.doctor import Doctor
 from app.schemas.doctor import DoctorIn, DoctorOut
+from app.models.user import User
+from app.utils.auth import get_current_active_user
 
 router = APIRouter(prefix="/doctors", tags=["doctors"])
 
 @router.post("/", response_model=DoctorOut)
-async def create_doctor(doctor: DoctorIn):
+async def create_doctor(
+    doctor: DoctorIn,
+    current_user: User = Depends(get_current_active_user)
+    ):
     doctor_obj = await Doctor.create(**doctor.dict(exclude_unset=True))
     return await DoctorOut.from_tortoise_orm(doctor_obj)
 
 @router.get("/", response_model=list[DoctorOut])
-async def get_all_doctors():
+async def get_all_doctors(current_user: User = Depends(get_current_active_user)):
     return await DoctorOut.from_queryset(Doctor.all())
 
 @router.get("/{doctor_id}", response_model=DoctorOut)
-async def get_doctor(doctor_id: int):
+async def get_doctor(doctor_id: int,  current_user: User = Depends(get_current_active_user)):
     try:
         return await DoctorOut.from_queryset_single(Doctor.get(id=doctor_id))
     except DoesNotExist:
@@ -25,12 +30,12 @@ async def get_doctor(doctor_id: int):
         )
 
 @router.put("/{doctor_id}", response_model=DoctorOut)
-async def update_doctor(doctor_id: int, doctor: DoctorIn):
+async def update_doctor(doctor_id: int, doctor: DoctorIn,  current_user: User = Depends(get_current_active_user)):
     await Doctor.filter(id=doctor_id).update(**doctor.dict(exclude_unset=True))
     return await DoctorOut.from_queryset_single(Doctor.get(id=doctor_id))
 
 @router.delete("/{doctor_id}")
-async def delete_doctor(doctor_id: int):
+async def delete_doctor(doctor_id: int, current_user: User = Depends(get_current_active_user)):
     deleted_count = await Doctor.filter(id=doctor_id).delete()
     if not deleted_count:
         raise HTTPException(
