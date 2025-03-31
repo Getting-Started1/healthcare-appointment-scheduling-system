5ebaf7e4837a053b73ed48fba618e6abb92e1035
Elvis-Munene <104641808+Elvis-Munene@users.noreply.github.com>
2025-04-01 00:08:55 +0300

    protects medical_record routes

--- app/routes/medical_record.py
+++ app/routes/medical_record.py
@@ -1,14 +1,17 @@
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status
 from tortoise.exceptions import DoesNotExist
 from app.models.medical_record import MedicalRecord
 from app.models.patient import Patient
 from app.models.appointment import Appointment
 from app.schemas.medical_record import MedicalRecordOut, MedicalRecordCreate
+from app.models.user import User
+from app.utils.auth import get_current_active_user
+
 
 router = APIRouter(prefix="/medical-records", tags=["medical_records"])
 
 @router.post("/", response_model=MedicalRecordOut)
-async def create_medical_record(record: MedicalRecordCreate):
+async def create_medical_record(record: MedicalRecordCreate, current_user: User = Depends(get_current_active_user)):
     # Check patient exists
     if not await Patient.filter(id=record.patient_id).exists():
         raise HTTPException(
@@ -33,11 +36,14 @@ async def create_medical_record(record: MedicalRecordCreate):
     return await MedicalRecordOut.from_tortoise_orm(record_obj)
 
 @router.get("/", response_model=list[MedicalRecordOut])
-async def get_all_medical_records():
+async def get_all_medical_records(current_user: User = Depends(get_current_active_user)):
     return await MedicalRecordOut.from_queryset(MedicalRecord.all())
 
 @router.get("/{record_id}", response_model=MedicalRecordOut)
-async def get_medical_record(record_id: int):
+async def get_medical_record(
+    record_id: int,
+    current_user: User = Depends(get_current_active_user)                         
+    ):
     try:
         return await MedicalRecordOut.from_queryset_single(MedicalRecord.get(id=record_id))
     except DoesNotExist:
